268a9c991ac4ac68724f43593f2eb2d4659ed629
Vladislav <vladbolkunovv@gmail.com>
2022-07-05 15:30:38 +0300

    PW fix, report upd

--- src/gui/LoggerWidget.py
+++ src/gui/LoggerWidget.py
@@ -7,12 +7,16 @@ class LoggerWidget(QGroupBox):
     def __init__(self):
         super().__init__()
         self.setTitle("Логи")
+
         lt = QVBoxLayout(self)
+
         self.text = QTextEdit()
-        lt.addWidget(self.text)
-        self.setLayout(lt)
         self.text.setReadOnly(True)
 
+        lt.addWidget(self.text)
+        self.setLayout(lt)
+
+    # Вывод лога в виджет
     def print(self, s):
         self.text.append(s)
 
--- src/gui/PopulationWidget.py
+++ src/gui/PopulationWidget.py
@@ -14,25 +14,28 @@ from src.model.core import Population, Town
 class PopulationWidget(QWidget):
     def __init__(self):
         super().__init__()
-        lt = QVBoxLayout(self)
-        splitter = QSplitter(self)
-        splitter.setOrientation(Qt.Orientation.Vertical)
 
         self.list = QListWidget()
         self.canvas = FigureCanvas(Figure())
         navbar = NavigationToolbar(self.canvas, self)
 
+        splitter = QSplitter(self)
+        splitter.setOrientation(Qt.Orientation.Vertical)
         splitter.addWidget(self.list)
         splitter.addWidget(self.canvas)
+
+        lt = QVBoxLayout(self)
         lt.addWidget(splitter)
         lt.addWidget(navbar)
         self.setLayout(lt)
 
-        self.list.model()
+        # Установка слота на сигнал изменения выбранного элемента
         self.list.currentRowChanged.connect(self.drawSolution)
-        self.towns: list[Town] = None
-        self.pop: Population = None
 
+        self.towns: list[Town]
+        self.pop: Population
+
+    # Слот отрисовки выбранного решения
     def drawSolution(self):
         x = [self.towns[i].x for i in self.pop[self.list.currentRow()]]
         y = [self.towns[i].y for i in self.pop[self.list.currentRow()]]
@@ -40,6 +43,7 @@ class PopulationWidget(QWidget):
         self.canvas.figure.subplots().plot(list(x) + [x[0]], list(y) + [y[0]], linestyle='--', marker='o')
         self.canvas.draw()
 
+    # Установка отображаемой популяции
     def setPopulation(self, pop: Population, towns: list[Town]):
         self.towns = towns
         self.pop = pop
@@ -48,4 +52,6 @@ class PopulationWidget(QWidget):
         for i in pop:
             self.list.addItem(str(i))
 
-        self.list.item(pop.index(pop.min())).setBackground(QColor('lime'))
+        self.list.setCurrentRow(0)
+        # self.list.item(pop.index(pop.min())).setBackground(QColor('lime'))
+        # self.list.setCurrentRow(pop.index(pop.min()))
